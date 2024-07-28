bd1506c5bc5ffab3cbfc4f04d8758b298811030f
suraj khot <surajkhot19082001@gmail.com>
2024-07-28 08:22:21 +0530

    binary search tracing

--- lib/interview_Questions/cws_06_binary_search.dart
+++ lib/interview_Questions/cws_06_binary_search.dart
@@ -15,14 +15,107 @@ void main() {
       break;
     }
     if (a[mid] < s) {
-      l++;
+      l = mid + 1;
     } else {
-      u--;
+      u = mid - 1;
     }
   }
   if (f == 1) {
-    print("$s  is found at position ${mid + 1}");
+    print("$s is found at position ${mid + 1}");
   } else {
     print("$s in not found in $a");
   }
 }
+
+/*
+trace pgm
+  List<int> a = [10, 20, 30, 40, 50];
+  s=10
+while:
+
+?itration 1
+    l=0 u=4
+    mid=l+u/2= 0+4/2= 2
+
+    if(a[2]==10){
+    30==10 
+    !false
+    }
+
+    if(a[2]<10){
+    !false
+    }
+    else{
+    u=2-1=1
+    }
+
+?itratuin 2
+l=0 u=1 s=10
+
+  mid=l+u/2=0+1/2=0
+  if(a[0]==10){
+  true
+  f=1
+  ?exit;
+  }
+ */
+
+/*
+trace pgm
+l=0 u=4 s=50
+?itration 1
+while:
+    (l<=u)0<=4  //?true
+    {
+      mid=l+u/2 =0+4/2 =2
+
+      if(a[mid]==s) a[2]==50 30==50 //!false
+      {
+        !not execute
+      }
+
+      if(a[mid]<s) a[2]<50 =30<50 //?true
+      {
+        l=mid+1 =2+1 =3
+      }
+      else
+      {
+        !not execute
+      }
+    }
+?itration 2
+l=3 u=4 s=50
+while:
+  (l<=u) 3<=4
+  {
+    mid=l+u/2 =3+4/2 =3
+    if(a[mid]==s) a[3]==s =40==50 //!false
+    {
+      !not execute
+    }
+
+    if(a[mid]<s) a[3]<50 =40<50  //?true
+    {
+      l=mid+1  l=3+1 =4
+    }
+    else
+    {
+      !not execute
+    }
+  } 
+?iteration 3:
+    l=4 u=4 s=50
+    while:
+      (l<=u) 4<=4 //?true
+      {
+        mid=l+u/2 4+4/2 =4
+        if(a[mid]==s) a[4]==50 50==50 //?true
+        {
+          f=1
+          break //?exit
+        }
+
+        !not execute
+      }
+print==>50 found at position 4
+*/
